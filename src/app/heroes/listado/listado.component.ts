95c65dc0c58187f9d3040ab1f6c2966d6f6e3a7a
CesarRivra <cesarrivcam@gmail.com>
2021-04-02 21:28:18 -0600

    First commit, Angular basics

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  //   styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent {
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor','Cap America'];
+  heroeBorrado: string ='';
+
+  borrarHeroe() {
+      console.log('borrando...');
+       this.heroeBorrado = this.heroes.shift() || '';
+  }
+}
